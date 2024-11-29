5b6604737d523330b75121cc96fdbdd37879c47b
matias <mati.delbono00@gmail.com>
2024-11-29 19:40:52 -0300

    DB Connection

---
+++ Sistema_Alumnos/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.example.Connections.HibernateUtil;
+import org.hibernate.Session;
+
+public class Main {
+    public static void main(String[] args) {
+        Session sesion= HibernateUtil.getSession();
+        if (sesion !=null){
+            System.out.println("Conexión realizada correctamente");
+        }
+        else {
+            System.out.println("No se ha podido realizar la conexión");
+    }
+}
+}
